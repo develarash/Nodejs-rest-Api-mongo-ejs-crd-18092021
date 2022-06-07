40d45366760cdf7aa765922f70dd6d6b5af1edc1
arashrahimi055 <develarash@gmail.com>
2022-06-08 02:15:07 +0430

    seconde commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express= require('express');
+const dotenv= require('dotenv');
+const app= express();
+dotenv.config({path:"config.env"})
+const PORT = process.env.PORT || 8080
+
+app.get('/',(req,res)=>{
+    res.send("Crud Application")
+})
+app.listen(PORT,()=>{console.log(`server is running on https://localhost: ${PORT}`)});
